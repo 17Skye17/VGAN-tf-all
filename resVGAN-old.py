6543255a65a0b98149e080021ea3cb8c54b47813
17skye17 <841577908@qq.com>
2018-05-29 02:02:41 +0800

    add G smoothing

--- resVGAN-old.py
+++ resVGAN-old.py
@@ -7,7 +7,7 @@ import h5py
 
 ######################## settings #################################
 z_size = 100
-batch_size = 1
+batch_size = 16
 IS_TRAIN = True
 starter_learning_rate = 0.0002
 beta1 = 0.5
@@ -16,21 +16,15 @@ A,B,C = 128,128,32
 channel = 3
 img_size = 128*128
 mode = "train"
-restore = True
-addnoise = True
-alpha = 1.0 # res rate
-n_size =  0   #noise size
+restore = False
+nsize = 0 # D noise size 
+alpha = 0.1 # res rate
 
-use_Gsmoothing = True
-G_smoothing = 0.999
-
-G_name = '../beach_G.npy'
-D_name = '../beach_D.npy'
-image_file_name = "./images1.0_noise10/"
-model_file_name = "./models1.0_noise10/"
+image_file_name = "./pretrain/resVGAN_image0.1_ns0/"
+model_file_name = "./pretrain/resmodel0.1_ns0/"
 dataset_filename = '/media/skye/b674e37c-7857-43ec-9527-dd932cb58935/beach128.hdf5'
-restore_path = './models1.0_noise/beach__49.ckpt'
-log_path = "./logs/1.0_noise10/"
+restore_path = '/home/skye/Desktop/VGAN-Tensorflow/pretrain/resmodel0.1_ns0/beach__49.ckpt'
+log_path = "/home/skye/Desktop/VGAN-Tensorflow/pretrain/logs/res0.1_ns0/"
 ###################################################################
 
 if os.path.exists(image_file_name) == False:
@@ -40,78 +34,65 @@ if os.path.exists(model_file_name) == False:
 if os.path.exists(log_path) == False:
 	os.mkdir(log_path)
 
-def Addnoise(img_array):
-	noise = np.random.normal(loc=np.mean(img_array[0]),scale=np.std(img_array[0]),size=(img_array.shape[1],img_array.shape[2],img_array.shape[3]))
-	max_noise = noise.max()
-	min_nosie = noise.min()
-	noise_normal = (noise-min_nosie)/((max_noise-min_nosie)+0.01) #[0,1] NUM
-	for i in range(n_size):
-		img_array = img_array + noise_normal
-#	addimg = noise_normal+img_array 
-	return img_array   #[32,128,128,3]
 
 def next_batch(data_array):
     length=data_array.shape[0] #assuming the data array to be a np arry
     permutations=np.random.permutation(length)
     idxs=permutations[0:batch_size]
-#    batch=np.zeros([batch_size, C, img_size*channel], dtype=np.float32)
-    batch = np.zeros([batch_size,C,A,B,channel],dtype=np.float32)
+    batch=np.zeros([batch_size, C, img_size*channel], dtype=np.float32)
     for i in range(len(idxs)):
-    #    batch[i,:]=data_array[idxs[i]].reshape(C,img_size*channel)
-	batch[i,:]=data_array[idxs[i]].reshape(C,A,B,channel)
-	if addnoise:
-		batch[i,:]=Addnoise(batch[i,:])  #[32,64,64,3]
+        batch[i,:]=data_array[idxs[i]].reshape(C,img_size*channel)
     return (batch / 255 - 0.5) / 0.5
 
-def static_net(z,reuse=None):
+def static_net(z):
 		static_input = tf.reshape(z,[batch_size, 1, 1, z_size])
 
-		static_dconv1 = deconv2d(static_input,[4,4,512,z_size],[batch_size, 4,4,512],[1,1,1,1],'VALID',name='st_deconv1',reuse=reuse)
-		static_dconv1 = batch_norm2(static_dconv1,scope='st_bn_dconv1',num_of_filters=512,reuse=reuse)
+		static_dconv1 = deconv2d(static_input,[4,4,512,z_size],[batch_size, 4,4,512],[1,1,1,1],'VALID',name='st_deconv1')
+		static_dconv1 = batch_norm2(static_dconv1,scope='st_bn_dconv1',num_of_filters=512)
 		static_dconv1 = tf.nn.relu(static_dconv1)
 
-		static_dconv2 = deconv2d(static_dconv1,[4,4,256,512],[batch_size, 8,8,256],[1,2,2,1],'SAME',name='st_deconv2',reuse=reuse)
-		static_dconv2 = batch_norm2(static_dconv2,scope='st_bn_dconv2',num_of_filters=256,reuse=reuse)
+		static_dconv2 = deconv2d(static_dconv1,[4,4,256,512],[batch_size, 8,8,256],[1,2,2,1],'SAME',name='st_deconv2')
+		static_dconv2 = batch_norm2(static_dconv2,scope='st_bn_dconv2',num_of_filters=256)
 		static_dconv2 = tf.nn.relu(static_dconv2)
 
-		static_dconv3 = deconv2d(static_dconv2,[4,4,128,256],[batch_size, 16,16,128],[1,2,2,1],'SAME',name='st_deconv3',reuse=reuse)
-		static_dconv3 = batch_norm2(static_dconv3,scope='st_bn_dconv3',num_of_filters=128,reuse=reuse)
+		static_dconv3 = deconv2d(static_dconv2,[4,4,128,256],[batch_size, 16,16,128],[1,2,2,1],'SAME',name='st_deconv3')
+		static_dconv3 = batch_norm2(static_dconv3,scope='st_bn_dconv3',num_of_filters=128)
 		static_dconv3 = tf.nn.relu(static_dconv3)
 
-		static_dconv4 = deconv2d(static_dconv3,[4,4,64,128],[batch_size, 32, 32,64],[1,2,2,1],'SAME',name='st_deconv4',reuse=reuse)
-		static_dconv4 = batch_norm2(static_dconv4,scope='st_bn_dconv4',num_of_filters=64,reuse=reuse)
+		static_dconv4 = deconv2d(static_dconv3,[4,4,64,128],[batch_size, 32, 32,64],[1,2,2,1],'SAME',name='st_deconv4')
+		static_dconv4 = batch_norm2(static_dconv4,scope='st_bn_dconv4',num_of_filters=64)
 		static_dconv4 = tf.nn.relu(static_dconv4)
 
-		static_dconv5 = deconv2d(static_dconv4,[4,4,3,64],[batch_size, 64,64, 3],[1,2,2,1],'SAME',name='st_deconv5',reuse=reuse)
+		static_dconv5 = deconv2d(static_dconv4,[4,4,3,64],[batch_size, 64,64, 3],[1,2,2,1],'SAME',name='st_deconv5')
 		static_dconv5 = tf.nn.tanh(static_dconv5)
 		return static_dconv5
 
 
-def video_net_and_mask(z,reuse=None):
+def video_net_and_mask(z):
 		video_input = tf.reshape(z,[batch_size, 1, 1, 1, z_size])
 
-		video_dconv1 = deconv3d(video_input,[2,4,4,512,z_size],[batch_size, 2, 4,4,512],[1,1,1,1,1],'VALID',name='video_deconv1',reuse=reuse)
-		video_dconv1 = batch_norm2(video_dconv1,scope='vd_bn_dconv1',num_of_filters=512,reuse=reuse)
+		video_dconv1 = deconv3d(video_input,[2,4,4,512,z_size],[batch_size, 2, 4,4,512],[1,1,1,1,1],'VALID',name='video_deconv1')
+		video_dconv1 = batch_norm2(video_dconv1,scope='vd_bn_dconv1',num_of_filters=512)
 		video_dconv1 = tf.nn.relu(video_dconv1)
 
-		video_dconv2 = deconv3d(video_dconv1,[4,4,4,256,512],[batch_size, 4, 8, 8, 256],[1,2,2,2,1],'SAME',name='video_deconv2',reuse=reuse)
-		video_dconv2 = batch_norm2(video_dconv2,scope='vd_bn_dconv2',num_of_filters=256,reuse=reuse)
+		video_dconv2 = deconv3d(video_dconv1,[4,4,4,256,512],[batch_size, 4, 8, 8, 256],[1,2,2,2,1],'SAME',name='video_deconv2')
+		video_dconv2 = batch_norm2(video_dconv2,scope='vd_bn_dconv2',num_of_filters=256)
 		video_dconv2 = tf.nn.relu(video_dconv2)
 
-		video_dconv3 = deconv3d(video_dconv2,[4,4,4,128,256],[batch_size, 8, 16, 16, 128],[1,2,2,2,1],'SAME',name='video_deconv3',reuse=reuse)
-		video_dconv3 = batch_norm2(video_dconv3,scope='vd_bn_dconv3',num_of_filters=128,reuse=reuse)
+		video_dconv3 = deconv3d(video_dconv2,[4,4,4,128,256],[batch_size, 8, 16, 16, 128],[1,2,2,2,1],'SAME',name='video_deconv3')
+		video_dconv3 = batch_norm2(video_dconv3,scope='vd_bn_dconv3',num_of_filters=128)
 		video_dconv3 = tf.nn.relu(video_dconv3)
 
-		video_dconv4 = deconv3d(video_dconv3,[4,4,4,64,128],[batch_size, 16, 32, 32, 64],[1,2,2,2,1],'SAME',name='video_deconv4',reuse=reuse)
-		video_dconv4 = batch_norm2(video_dconv4,scope='vd_bn_dconv4',num_of_filters=64,reuse=reuse)
+		video_dconv4 = deconv3d(video_dconv3,[4,4,4,64,128],[batch_size, 16, 32, 32, 64],[1,2,2,2,1],'SAME',name='video_deconv4')
+		video_dconv4 = batch_norm2(video_dconv4,scope='vd_bn_dconv4',num_of_filters=64)
 		video_dconv4 = tf.nn.relu(video_dconv4)
 
-		video_dconv5 = deconv3d(video_dconv4,[4,4,4,3,64],[batch_size, 32, 64, 64, 3],[1,2,2,2,1],'SAME',name='video_deconv5',reuse=reuse)
+		video_dconv5 = deconv3d(video_dconv4,[4,4,4,3,64],[batch_size, 32, 64, 64, 3],[1,2,2,2,1],'SAME',name='video_deconv5')
 		video_dconv5 = tf.nn.tanh(video_dconv5)
 
 		# mast out... (for the mast net)
 
-		mask_deconv5, mask_deconv5_weights = deconv3d(video_dconv4,[4,4,4,1,64], [batch_size, 32, 64, 64, 1],[1,2,2,2,1],'SAME',name='mask_deconv5',with_w=True,reuse=reuse)
+		mask_deconv5, mask_deconv5_weights = deconv3d(video_dconv4,[4,4,4,1,64], [batch_size, 32, 64, 64, 1],[1,2,2,2,1],'SAME',name='mask_deconv5',with_w=True)
 		mask_deconv5 = tf.nn.sigmoid(mask_deconv5)
 
 		l1_regularizer = tf.contrib.layers.l1_regularizer(scale=0.1, scope='mask_l1')
@@ -154,11 +135,11 @@ def load(sess,param):
 					except:
 					 	print 'Ignore ' + key
 
-def gen_video(z,reuse=None):
-	background = static_net(z,reuse=reuse)
+def gen_video(z):
+	background = static_net(z)
 	background = tf.tile(tf.reshape(background,[batch_size,1,64,64,3]),[1,32,1,1,1]) #replicate frames to change bg to [batchsize,32,64,64,3]
 
-	foreground,mask,penalty = video_net_and_mask(z,reuse=reuse)
+	foreground,mask,penalty = video_net_and_mask(z)
 	mask = tf.tile(mask,[1,1,1,1,3])#replicate frames to change mask to [batchsize,32,64,64,3]
 
 	video = foreground*mask + background*(1-mask) #dot multiply
@@ -190,25 +171,6 @@ def downscale3d(x):
 def pixel_norm(x, epsilon=1e-8):
     with tf.variable_scope('PixelNorm'):
         return x * tf.rsqrt(tf.reduce_mean(tf.square(x), axis=1, keepdims=True) + epsilon)
-
-def lerp(a, b, t):
-    with tf.name_scope('Lerp'):
-        return a + (b - a) * t
-		
-def setup_moving_average(Gs_gvars,G_gvars,beta=0.99):
-	with tf.name_scope("MovingAvg"):
-		ops = []
-		for i in range(len(Gs_gvars)):
-			var = tf.get_variable(Gs_gvars[i].name)
-#			var = Gs_gvars[i].value()
-			name = Gs_gvars[i].name
-			new_value = lerp(G_gvars[i].value(),var,beta)
-			ops.append(var.assign(new_value)) #can't assign
-			print "assign " +str(new_value)+"to"+str(var)
-		return tf.group(*ops)
-
-
-
 
 def get_comb_G(gen_videos):
 	#nearest neighbor 128x128
@@ -296,7 +258,9 @@ if restore == True:
 	saver.restore(sess, restore_path)
 else:
 	print "start to load pretrained model..."
+	G_name = './beach_G.npy'
 	param_G = np.load(G_name).item()
+	D_name = './beach_D.npy'
 	param_D = np.load(D_name).item()
 
 	load(sess,param_G)
@@ -310,20 +274,13 @@ if mode == "train":
 	for i in range(train_iters):
 		xtrain = next_batch(train_data)
 		xtrain = xtrain.reshape(-1,C,A,B,channel)
-			
-		z_sample = np.random.normal(1.0,size=(batch_size,z_size)) 
-		feed_dict={x:xtrain,z:z_sample}
-		if use_Gsmoothing:
-			Gs_gvars = g_vars
+		D_noise = np.random.normal(1.0,size=(nsize,C,A,B,channel))
 
+		xtrain = np.r_[xtrain[0:batch_size-nsize,:,:,:,:],D_noise[:,:,:,:,:]]
+		z_sample = np.random.normal(1.0,size=(batch_size,z_size)) 
+
+		feed_dict={x:xtrain,z:z_sample}
 		results = sess.run(fetches,feed_dict)
-		
-		if use_Gsmoothing:
-			G_gvars = g_vars
-			Gs_update_op = setup_moving_average(Gs_gvars,G_gvars,G_smoothing)
-			#update G weights
-			#use exponentiallly weighted averages
-		#	sess.run(Gs_update_op)
 
 		gen_D,real_D,d_loss,g_loss,_,_=results
 
@@ -334,11 +291,11 @@ if mode == "train":
 		if i%10 == 0:
 			print ("iter=%d : D_Loss: %f G_Loss: %f" % (i, d_loss, g_loss))
 
-		if (i+1)%50==0:
+		if (i+1)%10==0:
 			ckpt_file=model_file_name+"beach__"+str(i)+".ckpt"
 			print ("Model saved in file: %s" % saver.save(sess,ckpt_file))
 
-		if (i+1)%50==0:
+		if (i+1)%10==0:
 			sample = sess.run(comb_G,{z:z_sample})
 			output = image_file_name + str(i)
 			np.save(output,(sample*0.5+0.5)*255)
